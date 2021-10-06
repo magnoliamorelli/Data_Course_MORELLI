a1755ea54d2223ef00c483c064cedbd77a624453
magnoliamorelli <10939866@uvu.edu>
2021-10-06 13:01:09 -0600

    update

--- In_Class_Scripts/Finding_Files.R
+++ In_Class_Scripts/Finding_Files.R
@@ -52,3 +52,18 @@ ants <- read.csv("./Data/thatch_ant.csv")
 list.dirs(path = "Data", full.names = TRUE, recursive = TRUE)
 
 readLines("./Data/data-shell/writing/thesis")
+
+
+
+
+
+list.files(recursive = TRUE, full.names = False, pattern = "^b")
+
+
+creatures <- list.files(path = "Data/data-shell/creatures", full.names = TRUE)
+basilisk <- readLines(creatures[1])
+
+
+basilisk[5:10]
+stringr::str_flatten(basilisk[5:10])
+
--- In_Class_Scripts/Tidying_Excel.R
+++ In_Class_Scripts/Tidying_Excel.R
@@ -1,3 +1,68 @@
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(plotly)
+
+# Tidying Data:
+# every column = single variable (treatment type)
+# every row = single observation (sample ID)
+
+# Never change original data, always write code to change in R
+# When doing dates in Excel use ``back tick first or 3 columns (year,month,day)
+
+df <- read_xlsx("./Data/wide_data_example.xlsx")
+colnames(df) <- make_clean_names(colnames(df)) #changes name to clean
+
+
+df$treatment_1 %>% unique() # tells you if there is something unique ex: ? vs ??, might have to make 2 rules to clean
+lapply(df,unique) # this does unique for each column
+# doing that function that gives all plots, will help you pick out stuff that is wrong, maybe do that first when getting data?
+
+df$treatment_1[df$treatment_1 == "?"] <- NA # changes ? to NA
+df$treatment_1 <- as.numeric(df$treatment_1) # changes column to numeric
+
+
+
+df %>% 
+  pivot_longer(cols = c("treatment_1","treatment_2"), # making variables into one column
+               names_to = "Treatment",
+               values_to = "Brain_Activity") # changing column names
+# OR
+
+df_tidy <- df %>% 
+  pivot_longer(starts_with("treatment"))
+
+
+
+
+
+
+# More mutating
+
+
+library(tidyverse)
+library(palmerpenguins)
+
+df <- penguins
+
+p <- df %>% 
+  mutate(body_mass_kg=round(body_mass_g/1000,0)) %>% 
+  mutate(chonk = case_when(body_mass_g > 4500 ~ "chonky",
+                           is.na(body_mass_g) ~ "NA",
+                           TRUE ~ "Not Chonky")) %>%   # New column chonk, if this is >4500, it is chonky, if not it is not chonky
+  select(body_mass_g,body_mass_kg,everything()) %>%   # reordering columns
+  arrange(chonk,desc(body_mass_g)) %>% # arranging rows first by chonk then by body_mass_g
+  ggplot(aes(x=chonk,y=body_mass_g,color=sex)) + geom_boxplot()
+p + theme_minimal()
+
+ggplotly(p) # plotly is good for interactive graphs
+
+
+
+
+
+
+
 # do this at home
 
 library(tidyverse)
@@ -62,8 +127,4 @@ full %>%
 ggsave("", width = 20, height = 10, dip = 100) # this will save plots, can change dimensions or into pdf, png etc
 
 
-
-
-
-
-
+  
