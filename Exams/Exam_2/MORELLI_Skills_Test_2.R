91047e402a0fbb40b44732ac5344b6e6c5a0769a
magnoliamorelli <10939866@uvu.edu>
2021-10-11 14:53:36 -0600

    update

--- Exams/Exam_2/MORELLI_Skills_Test_2.R
+++ Exams/Exam_2/MORELLI_Skills_Test_2.R
@@ -21,12 +21,13 @@ dat_tidy <- dat %>%
   pivot_longer(starts_with("U5MR"),
                names_to = "Year",
                values_to = "Mortality_Rate",
-               names_prefix = "U5MR.")
+               names_prefix = "U5MR.",
+               names_transform = list(Year=as.numeric))
 
 # Fig 2
 dat_tidy %>% 
   filter(!is.na(Mortality_Rate)) %>% 
-  ggplot(aes(x=as.numeric(Year),y=Mortality_Rate,color=Continent)) +
+  ggplot(aes(x=Year,y=Mortality_Rate,color=Continent)) +
   geom_point() +
   labs(y='MortalityRate',
        x="Year")
@@ -35,12 +36,19 @@ ggsave("MORELLI_Fig_2.jpg", plot = last_plot())
 
 # Fig 3
 dat_tidy %>% 
-  mutate(Mean_Mortality_Rate=)
+  group_by(Continent,Year) %>% 
+  summarise(Mean_Mortality_Rate = mean(Mortality_Rate)) %>% 
+  ggplot(aes(x=Year,y=Mean_Mortality_Rate,color=Continent))+
+  geom_line() +
+  labs(x="Year",
+       y="Mean Mortality Rate (deaths per 1000 live births")
+
+ggsave("MORELLI_Fig_3.jpg", plot = last_plot())
   
 
 # Fig 4
 dat_tidy <- dat_tidy %>% 
-  mutate(prop_morality=dat_tidy$Mortality_Rate/1000)
+  mutate(prop_morality=dat_tidy$Mortality_Rate/1000) 
 dat_tidy %>% 
   ggplot(aes(x=Year,y=prop_morality)) +
   geom_point(color="blue",alpha=.5,size=.90) +
--- New_Assignments/Assignment_6/Assignment_6.R
+++ New_Assignments/Assignment_6/Assignment_6.R
@@ -12,21 +12,18 @@ dat <- dat %>%
   pivot_longer(cols = c("hr_24","hr_48","hr_144"),
                values_to = "absorbance",
                names_to = "time",
-               names_prefix = "hr_")
-
-envi_type <- dat %>% 
+               names_prefix = "hr_",
+               names_transform = list(time=as.numeric))
+dat <- dat %>% 
+  filter(dat$dilution == 0.1) %>% 
   mutate(type = case_when(sample_id == "Clear_Creek" ~ "water",
                           sample_id == "Waste_Water" ~ "water",
-                           is.na(sample_id) ~ "NA",
-                           TRUE ~ "soil"))
-
-
-
+                          is.na(sample_id) ~ "NA",
+                          TRUE ~ "soil")) 
 # Plot 1
 
-dil_mod <- envi_type[envi_type$dilution == 0.1,]
-dil_mod %>% 
-  ggplot(aes(x=as.numeric(time),y=absorbance,color=type)) +
+dat %>% 
+  ggplot(aes(x=time,y=absorbance,color=type)) +
   geom_smooth(se=F) +
   facet_wrap(~substrate) +
   labs(subtitle = "Just dilution 0.1",
@@ -41,14 +38,36 @@ dil_mod %>%
 # I could not figure out gganimate. I spent maybe 5 hours on it
 # This is all I got 
 
-sub <- dat[dat$substrate == "Itaconic Acid",]
-sub %>%
-  ggplot(aes(x=as.numeric(time),y=absorbance,color=sample_id)) +
-  geom_smooth(method = "lm", se=F) +
+p <- dat %>% 
+  filter(substrate == "Itaconic Acid") %>% 
+  group_by(dilution,sample_id,time) %>% 
+  summarise(Mean_Absorbance = mean(absorbance)) %>% 
+  ggplot(aes(x=time,y=Mean_Absorbance, color=sample_id)) +
+  geom_line() +
   facet_wrap(~dilution) +
-  labs(subtitle = "Itaconic Acid",
-       x="Time",
-       y="Mean_absorbance") +
-  labs(color="Sample ID") +
-  theme_minimal() 
+  theme_minimal()
+
+p + transition_reveal(time)
+
+
+
+
+# In class modeling, Model Tween 80 ####
+
+
+dat %>% 
+  filter(dilution == 0.1) %>% 
+  filter(substrate == "Tween 80 ") %>% 
+  ggplot(aes(x=sqrt(absorbance))) +
+  geom_density()
+
+dat %>% 
+  filter(dilution == 0.1) %>% 
+  filter(substrate == "Tween 80 ") %>% 
+glm(data=.,
+    formula = absorbance ~ type * time) %>% # if + it is type and time if * it will be type over time
+  summary()
+
+# go to simple_regression in inclassscripts for more help
+
 
